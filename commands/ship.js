b27040a5afbf5c880385e8790c40c26a353db83c
Will Paceley <pace0033@algonquinlive.com>
2022-08-03 21:29:54 -0400

    add additional fields and update styling

--- commands/ship.js
+++ commands/ship.js
@@ -2,6 +2,14 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 
+const colors = {
+  Red: "#E74C40",
+  Blue: "#1F61AB",
+  Green: "#488025",
+  Yellow: "#E2B32D",
+  White: "#FFFFFF",
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ship")
@@ -25,7 +33,7 @@ module.exports = {
       // Fetch the properties we need for the embed
       const ship = await fetch(`https://voidrunners.io/api/ships/${id}`).then(
         (response) => {
-          // Check if the API returns a Gawd for the ID specified
+          // Check if the API returns a ship for the ID specified
           if (!response.ok) {
             throw new Error(`No Genesis Fleet ship found with ID ${id}`);
           }
@@ -33,26 +41,31 @@ module.exports = {
         }
       );
 
+      // Get the hull from parts array in ship API data
+      const hull = ship.parts.find((part) => part.type === "hull").value;
+
       // Create embed for message reply
       const embed = new MessageEmbed()
-        .setColor("#FFFFFF")
+        .setColor(colors[ship.stats.registered_color])
         .setTitle(ship.name)
         .setURL(ship.external_url)
         .addFields(
-          { name: "Class", value: ship.stats.ship_class },
+          { name: "Class", value: ship.stats.ship_class, inline: true },
+          { name: "Hull", value: hull, inline: true },
+          { name: "Colorway", value: ship.stats.colorway, inline: true },
           {
             name: "Capacity",
-            value: ship.stats.capacity.toString(),
+            value: `${ship.stats.capacity} (${ship.stats.rank_capacity} rank)`,
             inline: true,
           },
           {
             name: "Efficiency",
-            value: ship.stats.efficiency.toString(),
+            value: `${ship.stats.efficiency} (${ship.stats.rank_efficiency} rank)`,
             inline: true,
           },
           {
             name: "Speed",
-            value: ship.stats.speed.toString(),
+            value: `${ship.stats.speed} (${ship.stats.rank_speed} rank)`,
             inline: true,
           }
         )
